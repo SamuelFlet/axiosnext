49ccef0b5a761387f272b93336ce264f13c8b764
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-02-17 10:43:13 -0500

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,9 @@ import {
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
-import MainFeaturedPost from "./MainFeaturedPost";
-import Header from "./Header";
+import MainFeaturedPost from "../components/MainFeaturedPost";
+import Header from "../components/Header";
 
 const client = new ApolloClient({
   uri: "https://practicedbapp.herokuapp.com/graphql",
@@ -28,6 +27,7 @@ const sections = [
   { title: "About", url: "#" },
   { title: "Personal Website", url: "#" },
 ];
+
 
 const wowie = gql`
   query {
