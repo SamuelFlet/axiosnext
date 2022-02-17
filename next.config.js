3ed0a86f715253c13ec2ed7b3d2c9f56b213b192
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-02-17 10:29:17 -0500

    haha

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://blog--images3.s3.us-west-2.amazonaws.com',
+    domains: ['blog--images3.s3.us-west-2.amazonaws.com'],
+  },
 }
 
 module.exports = nextConfig
